ceeee94d079ea6149c4211b99390aa06260b81e0
ayushr2 <ayushr2@illinois.edu>
2017-02-25 18:58:55 -0600

    tabs

--- mainsite/static/js/functions.js
+++ mainsite/static/js/functions.js
@@ -1,15 +1,41 @@
-openHoursWebsite = function(){
+function openHoursWebsite() {
+    "use strict";
     window.open("https://campusrec.illinois.edu/facilities/hours-of-operation/");
 }
 
-openArcWebsite = function(){
+function openArcWebsite() {
+    "use strict";
     window.open("https://campusrec.illinois.edu/facilities/activities-and-recreation-center-arc/");
 }
 
-openCrceWebsite = function(){
+function openArcWebsite() {
+    "use strict";
     window.open("https://campusrec.illinois.edu/facilities/campus-recreation-center-east-crce/");
 }
 
-openRecreationWebsite = function(){
+function openRecreationWebsite() {
+    "use strict";
     window.open("https://campusrec.illinois.edu/");
+}
+
+function openChat(evt, chatName) {
+    "use strict";
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i += 1) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i += 1) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(chatName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
